999ffe527c37dab6176af19c87e11fdc4415c0dd
Prosper Ikechukwu <mrhendrix62@gmail.com>
2023-04-18 19:44:21 +0100

    FEAT: Working on SideBar Navigation

--- src/pages/sidebar/SideBar.jsx
+++ src/pages/sidebar/SideBar.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import styles from './SideBar.module.css';
-import { DashBoardIcon, LighteningWelcome } from '../../components/icons/Icons';
+import {
+	DashBoardIcon,
+	LighteningWelcome,
+	SettingsIcon,
+	LighteningIcon,
+	UsersIcon,
+	AddUserIcon,
+	AffliateIcon,
+} from '../../components/icons/Icons';
 import IconText from '../../components/iconText/IconText';
 
 const SideBar = () => {
@@ -13,11 +21,34 @@ const SideBar = () => {
 
 			<div className={styles.navigations}>
 				<IconText text="Dashboard" space={9}>
-					{' '}
-					<DashBoardIcon size={18} fill='white' />{' '}
+					<DashBoardIcon size={18} fill="white" />
 				</IconText>
+
+				<IconText text="Settings" space={9}>
+					<SettingsIcon size={18} fill="white" />
+				</IconText>
+
+				<IconText text="Activities" space={9}>
+					<LighteningIcon size={18} fill="white" />
+				</IconText>
+
+				<IconText text="Users" space={9}>
+					<UsersIcon size={18} fill="white" />
+				</IconText>
+
+				<IconText text="Added user" space={9}>
+					<AddUserIcon size={18} fill="white" />
+				</IconText>
+
+				<IconText text="Affliate" space={9}>
+					<AffliateIcon size={18} fill="white" />
+				</IconText>
+				
+				<div className={styles.line} />
 			</div>
+
 		</div>
+
 	);
 };
 
